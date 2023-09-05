ed05d84a000093f9033aff602550f9da64625114
Xavier R. Guérin <xguerin@users.noreply.github.com>
2023-09-05 22:02:51 +0200

    Instantiate api::Server as a smart pointer is ssl::Server

--- include/tulips/ssl/Server.h
+++ include/tulips/ssl/Server.h
@@ -16,11 +16,11 @@ public:
          const ssl::Protocol type, std::string_view cert, std::string_view key);
   ~Server() override;
 
-  inline Status run() override { return m_server.run(); }
+  inline Status run() override { return m_server->run(); }
 
   inline Status process(const uint16_t len, const uint8_t* const data) override
   {
-    return m_server.process(len, data);
+    return m_server->process(len, data);
   }
 
   Status close(const ID id) override;
@@ -32,12 +32,12 @@ public:
 
   inline void listen(const stack::tcpv4::Port port, void* cookie) override
   {
-    m_server.listen(port, cookie);
+    m_server->listen(port, cookie);
   }
 
   inline void unlisten(const stack::tcpv4::Port port) override
   {
-    m_server.unlisten(port);
+    m_server->unlisten(port);
   }
 
   void* onConnected(ID const& id, void* const cookie, uint8_t& opts) override;
@@ -62,7 +62,7 @@ private:
   interface::Server::Delegate& m_delegate;
   system::Logger& m_log;
   transport::Device& m_dev;
-  api::Server m_server;
+  std::unique_ptr<api::Server> m_server;
   void* m_context;
 };
 
