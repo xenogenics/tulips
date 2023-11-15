5912ae8f01aff71c49b89f2e22e953ad83d34432
Xavier R. Guérin <xguerin@users.noreply.github.com>
2023-11-15 18:23:04 +0100

    Fix build on M-class ARM processors (#84)

--- include/tulips/system/Utils.h
+++ include/tulips/system/Utils.h
@@ -6,13 +6,28 @@
 
 namespace tulips::system::utils {
 
+#ifdef __aarch64__
+
+inline uint32_t
+log2(const uint32_t x)
+{
+  uint32_t y, z;
+  asm("rbit %w1, %w0" : "=r"(y) : "r"(x));
+  asm("clz %w1, %w0" : "=r"(z) : "r"(y));
+  return x;
+}
+
+#else
+
 inline uint32_t
 log2(const uint32_t x)
 {
   uint32_t y;
-  asm("\tbsr %1, %0\n" : "=r"(y) : "r"(x));
+  asm("bsr %1, %0" : "=r"(y) : "r"(x));
   return y;
 }
+
+#endif
 
 void join(std::vector<std::string> const& r, const char d, std::string& s);
 void split(std::string_view s, const char d, std::vector<std::string>& r);
